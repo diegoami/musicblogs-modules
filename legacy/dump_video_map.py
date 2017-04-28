00605dcd89fbccfe9e3b04556dcdfc1b6f0ead76
Diego Amicabile <diego.amicabile@gmail.com>
2017-04-28 20:49:14 +0200

    Move files

--- legacy/dump_video_map.py
+++ legacy/dump_video_map.py
@@ -1,6 +1,6 @@
 import csv
 import sys
-
+import os
 import youtube_tools
 
 import blogspot_tools
@@ -10,31 +10,35 @@ import datetime
 
 import argparse
 
-def fill_map(blogId, apikey ):
-    blogmap = {}
+def fill_map(blogId, apikey, blogMap, debug=False ):
+
     for blog_post in blogspot_tools.iterate_blog_posts(blogId, apikey):
         for id, title, videoId in blogspot_tools.iterate_title_and_videos(blog_post, apikey):
-            blogmap[id] = (title, videoId, datetime.date.fromtimestamp(0))
-
-    return blogmap
+            if id not in blogMap or blogMap[id][1] != videoId:
+                debug and print("Updating "+id+" to "+videoId)
+                blogMap[id] = (title, videoId, datetime.date.fromtimestamp(0))
+            else:
+                debug and print("Ignoring " + id + " and " + videoId)
+    return blogMap
 
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--blogId')
 parser.add_argument('--apikey')
-parser.add_argument('--inputfile')
 parser.add_argument('--outputfile')
+parser.add_argument('--debug')
 
 args = parser.parse_args()
+debug = True if args.debug else False
+initialMap = {}
+if os.path.isfile(args.outputfile):
+    with open(args.outputfile, 'rb') as handle:
+        initialMap = pickle.load(handle)
 
 
-fileoutname = args.outputfile
-blogId =  args.blogId
-apikey =  args.apikey
-
-videoMap = fill_map(blogId, apikey )
-with open(fileoutname, 'wb') as handle:
+videoMap = fill_map( args.blogId, args.apikey, initialMap ,debug=args.debug )
+with open(args.outputfile, 'wb') as handle:
     pickle.dump(videoMap, handle,  protocol=pickle.HIGHEST_PROTOCOL)
 
 
