7953f1e0ff38c42f3dcb843374cfde6cc15ca459
Diego Amicabile <diego.amicabile@gmail.com>
2019-02-04 00:06:20 +0100

    using now blogclient library

--- tools/blog_amara_tool.py
+++ tools/blog_amara_tool.py
@@ -1,18 +1,16 @@
-from tools import tool_blog_client
-
 from amaraapi import AmaraTools, AmaraVideo
-
+from blogspotapi import BlogClient
 
 
 class BlogAmaraTool:
-    def __init__(self, amara_headers):
+    def __init__(self, amara_headers, config_file):
         self.amara_headers = amara_headers
         self.amara_tools = AmaraTools(self.amara_headers)
+        self.blog_client = BlogClient(config_file)
 
     def post_video_from_blog(self, blogId, postId, language_code):
-        service, flags = tool_blog_client.login()
-        videoId = tool_blog_client.extract_video(blogId, postId, service.posts())
-        lyrics = tool_blog_client.retrieve_lyrics(service, blogId, postId)
+        videoId = self.blog_client.extract_video(blogId, postId)
+        lyrics = self.blog_client.retrieve_lyrics(blogId, postId)
         conv_lyrics = self.amara_tools.convert_to_lyrics(lyrics)
         amara_id = self.amara_tools.get_video_id(video_url='https://youtu.be/'+videoId, language_code=language_code)
         amara_video = AmaraVideo(self.amara_headers, amara_id)
@@ -20,8 +18,7 @@ class BlogAmaraTool:
         amara_video.post_subtitles(language_code, conv_lyrics)
 
     def test_add_amaratags(self, blogId, postId, language_code):
-        service, flags = tool_blog_client.login()
-        tool_blog_client. insert_amara_tags(blogId, postId, service.posts(), language_code)
+        self.blog_client.insert_amara_tags(blogId, postId, language_code)
 
     def subtitles_workflow(self, blogId, postId, language_code):
         self.test_add_amaratags(blogId, postId, language_code)
