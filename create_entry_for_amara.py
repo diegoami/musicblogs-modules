f759eb43348764af9e9bd7084f77ad11ecba6d1c
Diego Amicabile <diego.amicabile@gmail.com>
2019-02-10 20:58:55 +0100

    Amara / Blogspot updates

--- create_entry_for_amara.py
+++ create_entry_for_amara.py
@@ -1,11 +1,8 @@
 from tools.blog_amara_tool import BlogAmaraTool
-import os
+from amara.amara_env import amara_headers
 from oauth2client.tools import argparser
 
 from tools import tool_blog_client
-from dotenv import load_dotenv
-load_dotenv()
-
 
 argparser.add_argument('--blogId')
 argparser.add_argument('--postId')
@@ -15,13 +12,7 @@ args = argparser.parse_args()
 service, flags = tool_blog_client.login()
 blogId = args.blogId
 language_code = args.language_code
-
-
 postId = args.postId
-amara_headers={
-    "X-api-username": os.getenv('AMARA-X-api-username'),
-    "X-api-key": os.getenv('AMARA-X-api-key')
-}
 
 blog_amara_tool = BlogAmaraTool(amara_headers)
 blog_amara_tool.subtitles_workflow(blogId, postId, language_code)
