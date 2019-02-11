3295d90abad1ff78e8f1d37d831682e2fcc7996a
Diego Amicabile <diego.amicabile@gmail.com>
2019-02-11 23:32:43 +0100

    removed old guys

--- create_entry_for_amara.py
+++ create_entry_for_amara.py
@@ -2,14 +2,13 @@ from tools.blog_amara_tool import BlogAmaraTool
 from amara.amara_env import amara_headers
 from oauth2client.tools import argparser
 
-from tools import tool_blog_client
+from tools.blog_amara_tool import BlogAmaraTool
 
 argparser.add_argument('--blogId')
 argparser.add_argument('--postId')
 argparser.add_argument('--language_code')
 
 args = argparser.parse_args()
-service, flags = tool_blog_client.login()
 blogId = args.blogId
 language_code = args.language_code
 postId = args.postId
